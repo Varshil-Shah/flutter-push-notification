407bc44d077687f840b8908be1f5ecd21391c1ac
Varshil-Shah <varshilashah09@gmail.com>
2022-02-24 17:29:06 +0530

    :bricks: Design the structure of push notification

---
+++ lib/models/push_notification.dart
@@ -0,0 +1,13 @@
+class PushNotification {
+  String? title;
+  String? body;
+  String? dataTitle;
+  String? dataBody;
+
+  PushNotification({
+    this.body,
+    this.dataBody,
+    this.title,
+    this.dataTitle,
+  });
+}
